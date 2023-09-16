df4acb67e0c51df24f97c0b2ebf7401139c45a0a
DelgaSol <jessica.delgadosolano@ucr.ac.cr>
2023-09-16 11:28:40 -0600

    banner
    banner

---
+++ dist/js/components/banner.js
@@ -0,0 +1,14 @@
+app.component('banner', {
+    template:
+      /*html*/
+      `    <!--footer-->
+      <div class="card">
+      <img src="./imgs/banner.png" class="card-img banner" alt="banner">
+      <div class="banner-p col-6">
+        <h5 class="card-title banner-title">¡Bienvenido a EcoWave!</h5>
+        <p class="card-text banner-text">Somos una página para encontrar puntos de reciclaje y campañas cerca de tu localidad</p>
+      </div>
+    </div>`
+
+
+  });
