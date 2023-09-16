af60551daf45d9af8f1dec582aa931e97365a6e3
DelgaSol <jessica.delgadosolano@ucr.ac.cr>
2023-09-15 23:43:50 -0600

    footer
    footer, media-query

---
+++ dist/js/components/Footer.js
@@ -0,0 +1,26 @@
+app.component('footer-nav', {
+    template:
+      /*html*/
+      `    <!--footer-->
+    <nav class="nav d-flex flex-column footer-bg">
+    <a class="navbar-brand logo-mobile color-header" href="#">
+              <img src="./imgs/logoBlanco.png" class="logo img-fluid d-inline-block footer-logo" alt="logotipo">
+    </a>
+
+
+    <div class="d-flex justify-content-start m-footer">
+    <a class="nav-link footer-links m-footer-txt" href="#">Inicio</a>
+    <a class="nav-link footer-links m-footer-txt" href="#">Blog</a>
+    <a class="nav-link footer-links" href="#">perfil</a>
+    </div>
+
+    <div class="d-flex justify-content-center">
+        <div class="d-flex flex-row">
+            <p class="footer-txt">©2023 EcoWave.</p>
+            <p class="footer-txt">Todos los Derechos Reservados.</p>
+        </div>
+    </div>
+    </nav>`
+
+
+  });
