dd682075fa933da48d59cc903e1efcfbc4f18787
malauMiranda <malaumiranda6@gmail.com>
2023-09-16 19:13:28 -0600

    Login y Signup
    Creación de dos componentes: uno de login y otro de signup; cada uno con sus respectivos archivos html

---
+++ dist/js/components/Login.js
@@ -0,0 +1,49 @@
+app.component('login', {
+
+
+  template:
+    /*html*/
+    `  
+    <section class="login-reg-bg"> 
+    <div class="card card-login">
+      <div class="card-body text-center">
+        <form>
+
+          <h5 class="card-title login-title">Iniciar Sesión</h5>
+          <div class="d-flex justify-content-center align-items-center flex-wrap">
+            <div class="mb-2">
+              <h1 class="login-text text-start">Correo Electrónico:</h1>
+            </div>
+            <div class="mb-2">
+              <input type="email" class="form-control login-input" id="1" aria-describedby="emailHelp">
+            </div>
+          </div>
+
+
+          <div class="d-flex justify-content-center align-items-center flex-wrap">
+            <div class="mb-2">
+              <h1 class="login-text text-start">Contraseña:</h1>
+            </div>
+            <div class="mb-2">
+              <input type="password" class="form-control login-input ms-lg-5" id="1" aria-describedby="password">
+            </div>
+          </div>
+
+            <div class="d-flex justify-content-center align-items-center mt-5 mb-5">
+            <a href="home.html" class="btn-access">Acceder</a>
+            </div>
+        </form>
+
+
+
+
+        
+        <a href="signup.html" class="card-link mb-5">Crear Cuenta</a>
+        <a href="#" class="card-link mb-5">¿Olvidaste tu Contraseña?</a>
+      </div>
+    </div>
+  </section>
+
+      `
+
+});
