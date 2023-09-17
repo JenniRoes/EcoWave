32b06567ca7f08b5b17f69d1b0ae49abc3fc9388
andressr2709 <andres76579@gmail.com>
2023-09-17 15:02:04 -0600

    componente sidecard

---
+++ dist/js/components/sidecard.js
@@ -0,0 +1,15 @@
+app.component('sidecard', {
+    template:
+      /*html*/
+      `    
+      <div>
+  <b-card title="Productos Hechos de Material Reciclado">
+    <b-card-text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </b-card-text>
+
+   
+  </b-card>
+</div>
+      `
+  });
