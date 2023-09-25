99c84e5f085d543c54bd7803d62ddb21f2796bfa
unknown <jenniroes2801@gmail.com>
2023-09-24 20:02:34 -0600

    El responsive mobile de Home está listo

--- dist/js/components/Footer.js
+++ dist/js/components/Footer.js
@@ -1,26 +1,25 @@
 app.component('footer-nav', {
-    template:
-      /*html*/
-      `    <!--footer-->
+  template:
+    /*html*/
+    `    <!--footer-->
     <footer class="d-flex flex-column footer-bg">
-    <a class="navbar-brand logo-mobile color-header" href="#">
-              <img src="./imgs/logoBlanco.png" class="logo img-fluid d-inline-block footer-logo" alt="logotipo">
-    </a>
+    <div class="col">
+        <img src="./imgs/logoBlanco.png" class="logo img-fluid footer-logo" alt="logotipo">
 
-
-    <div class="footer-item m-footer">
-    <a class="nav-link footer-links m-footer-txt" href="./home.html">Inicio</a>
-    <a class="nav-link footer-links m-footer-txt" href="#">Blog</a>
-    <a class="nav-link footer-links" href="#">Perfil</a>
-    </div>
-
-    <div class="d-flex justify-content-center">
-        <div class="d-flex flex-row">
-            <p class="footer-txt">©2023 EcoWave.</p>
-            <p class="footer-txt">Todos los Derechos Reservados.</p>
+        <div class="footer-item m-footer">
+          <a class="nav-link footer-links m-footer-txt" href="./home.html">Inicio</a>
+          <a class="nav-link footer-links m-footer-txt" href="#">Blog</a>
+          <a class="nav-link footer-links" href="#">Perfil</a>
         </div>
-    </div>
+      </div>
+
+      <div class="d-flex justify-content-center">
+          <div class="d-flex flex-row">
+              <p class="footer-txt">©2023 EcoWave.</p>
+              <p class="footer-txt">Todos los Derechos Reservados.</p>
+          </div>
+      </div>
     </footer>`
 
 
-  });
+});
