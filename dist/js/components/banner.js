3071369f46b20df0f105c0c7be40cc77816408e3
unknown <jenniroes2801@gmail.com>
2023-09-30 22:04:59 -0600

    Modificación en el componente Banner para que reciba props

--- dist/js/components/banner.js
+++ dist/js/components/banner.js
@@ -1,14 +1,26 @@
 app.component('banner', {
+  props: {
+    imagen: {
+      type: String,
+      default: "Imagen banner"
+    },
+    titulo: {
+      type: String,
+      default: "¡Bienvenido!"
+    },
+    descripcion: {
+      type: String,
+      default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem est lectustelluspellentesque at purus. Dictumst blandit odio orci elit non donec sodales. Nullam quampharetra,eleifendconsequat velit id euismod.Mauris nisi, suspendisse tristique ac urna. Fermentum sit enim porttitor et vitae. Vestibulumcraspellentesque interdum sit ullamcorper. Sed purus fringilla pellentesque interdum volutpatelitpulvinarplacerat."
+  }
+  },
     template:
       /*html*/
       `    <!--footer-->
       <div class="card">
-      <img src="./imgs/banner.png" class="card-img banner" alt="banner">
+      <img :src="imagen" class="card-img banner" alt="banner">
       <div class="banner-p col-6">
-        <h5 class="card-title banner-title">¡Bienvenido a EcoWave!</h5>
-        <p class="card-text banner-text">Somos una página para encontrar puntos de reciclaje y campañas cerca de tu localidad</p>
+        <h5 class="card-title banner-title">{{titulo}}</h5>
+        <p class="card-text banner-text">{{descripcion}}</p>
       </div>
     </div>`
-
-
   });
