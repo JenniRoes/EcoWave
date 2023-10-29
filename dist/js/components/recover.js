87ca72c489d96810372f3e70cc4266444b3bd1de
unknown <jenniroes2801@gmail.com>
2023-10-29 16:24:30 -0600

    Funcionalidad recuperar contraseña completa

--- dist/js/components/recover.js
+++ dist/js/components/recover.js
@@ -1,49 +1,43 @@
 app.component('recover', {
+  methods: {
+    newPassword() {
+        const email = document.getElementById('email').value;
 
-
-    template:
+        axios.post('http://localhost/laravel_ecowave/example-app/public/api/recoverpassword', {
+            email: email,
+        })
+            .then(response => {
+                localStorage.setItem('password', response.data.password);
+                window.location.href = 'http://localhost/EcoWave/dist/newpassword.html';
+            })
+            .catch(error => {
+                console.log(error);
+                alert("No se encuentra registrado o ha ingresado los datos incorrectamente")
+            });
+    }
+},
+template:
       /*html*/
       `  
       <section class="login-reg-bg"> 
       <div class="card card-login">
         <div class="card-body text-center">
-          <form>
-
-          
+          <form> 
   
             <h5 class="card-title login-title">Recuperar Contraseña</h5>
-            
-            <div class="d-flex justify-content-center align-items-center flex-wrap">
-              <div class="mb-2">
-                <h1 class="login-text text-start">Código:</h1>
-              </div>
-              <div class="mb-2 ">
-                <input type="name" class="form-control login-input ms-lg-5" id="1" aria-describedby="nameHelp">
-              </div>
-            </div>
 
 
             <div class="d-flex justify-content-center align-items-center flex-wrap">
             <div class="mb-2">
-              <h1 class="login-text text-start">Nueva Contraseña:</h1>
+              <h1 class="login-text text-start">Ingrese su correo electrónico:</h1>
             </div>
             <div class="mb-2">
-              <input type="password" class="form-control login-input" id="1" aria-describedby="password">
+              <input type="text" class="form-control login-input" id="email" aria-describedby="password">
             </div>
           </div>
-  
-  
-            <div class="d-flex justify-content-center align-items-center flex-wrap">
-              <div class="mb-2">
-                <h1 class="login-text text-start">Confirmar Contraseña:</h1>
-              </div>
-              <div class="mb-2">
-                <input type="password" class="form-control login-input ms-lg-5" id="1" aria-describedby="passwordC">
-              </div>
-            </div>
-  
-              <div class="d-flex justify-content-center align-items-center mt-5 mb-5">
-              <a href="home.html" class="btn-access">Confirmar</a>
+
+          <div class="d-flex justify-content-center align-items-center mt-5 mb-5">
+              <a href="#" class="btn-access" @click="newPassword">Generar contraseña</a>
               </div>
           </form>
   
@@ -53,5 +47,4 @@ app.component('recover', {
     </section>
   
         `
-  
-  });
+});
