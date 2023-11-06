0b40799e70680ac05f1f1d2097ac7ec1a8700aed
unknown <jenniroes2801@gmail.com>
2023-11-05 22:45:33 -0600

    Funcionalidad buscar completa

--- dist/js/components/Search.js
+++ dist/js/components/Search.js
@@ -2,7 +2,6 @@ app.component('search', {
     components: {
         Navbar: './Navbar.js'
     },
-
     data() {
         return {
             results: [],
@@ -21,42 +20,43 @@ app.component('search', {
         getResults() {
             axios({
                 method: 'get',
-                url: 'http://localhost/laravel_ecowave/example-app/public/api/publicacion/searchbykeyword' + this.keyword
+                url: 'http://localhost/laravel_ecowave/example-app/public/api/publicacion/searchbykeyword?keyword=' + this.keyword
             })
-                .then(
-                    (response) => {
+                .then((response) => {
 
-                        this.results = response.data;
+                    let results = response.data.data;
 
-                        if (this.results.length > 0) {
-                            // Datos encontrados
-                            // Realiza acciones aquí
-                        }
-                    }
+                    results.forEach((element) => {
+                        this.results.push({
+                            id: element.id,
+                            title: element.title,
+                            description: element.description,
+                            created_at: element.created_at
+                        })
+                    });
+                }
                 )
                 .catch(
                     error => console.log(error)
                 );
-        }
+        },
     },
-    template: 
-    /*html*/
-    `
-        <div class="row mt-5">
-            <div v-if="results.length > 0">
-                <h4 class='ms-2 mt-3'>Results for <span class=''>{{ this.keyword }}</span></h4>
-            </div>
-
-            <div v-if="results.length === 0">
-                <h4 class='text-center mt-3'>No results for <span class=''>{{ keyword }}</span></h4>
-            </div>
+    template:
+        /* html */
+        `
+    <div class="row mt-5">
+    <div v-if="results != null">
+        <h4 class='ms-2 mt-3'>Resultados para <span class='fw-bolder card-link'>{{this.keyword}}</span></h4>
+    </div>
+    </div>
+    <div  class="row mt-5">
+        <div v-for="(item, index) in results" class="col mb-4">
+        <cards :image="item.image" :title="item.title"
+        :description="item.description" :data="item" :id="item.id"></cards>
         </div>
-
-        <div class="row mt-5">
-            <div v-for="(item, index) in results" class="col mb-4">
-                <cards :title="item.title" :subtitle="item.subtitle"
-                    :description="item.description" :author="item.author" :ubication="item.ubication" :data="item" :id="item.id"></cards>
-            </div>
-        </div>
+    </div>
+    <div v-if="results == ''">
+    <h4 class='text-center mt-3 mb-5 pb-5'>No hay resultados</h4>
+    </div>
     `
 });
