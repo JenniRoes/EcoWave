26df3de3e287f2b1a41fdf75acbad70fc3cc7250
unknown <jenniroes2801@gmail.com>
2023-11-16 22:04:26 -0600

    Funcionalidad artículos recomendados completa. Además, se rediseña la página home para una mayor legibilidad entre secciones

--- dist/js/components/sidecard.js
+++ dist/js/components/sidecard.js
@@ -1,14 +1,21 @@
 app.component('sidecard', {
+  props: {
+    title: {
+      type: String,
+      default: "Titulo de artículo"
+    },
+    description: {
+      type: String,
+      default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a erat eget magna interdum posuere."
+    }
+  },
   template:
     /*html*/
     `    
 <div>
     <b-card>
-      <p class="sidecard-title">Productos Hechos de Material Reciclado</p>
-      <b-card-text class="sidecard-subtitle"> 
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Sed a erat eget magna...
-      </b-card-text>
+      <a href="./blog.html" class="sidecard-title no-link">{{title}}</a>
+      <p class="sidecard-subtitle mt-3" >{{description}}</p>
     </b-card>
 </div>
       `
