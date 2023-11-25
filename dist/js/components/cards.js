0150675ac2e81b15e36a145b7b615d2a570332db
Jason Huertas <jasonhc.285@gmail.com>
2023-11-24 20:00:40 -0600

    Metodo Eliminar Post

--- dist/js/components/cards.js
+++ dist/js/components/cards.js
@@ -1,27 +1,50 @@
 app.component('cards', {
   props: {
-    fecha: {
+    created_at: {
       type: String,
       default: "Fecha de publicación"
     },
-    titulo: {
+    date: {
+      type: String,
+      default: "20/52/23"
+    },
+    title: {
       type: String,
       default: "Titulo de publicación"
     },
-    descripcion: {
+    description: {
       type: String,
       default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a erat eget magna interdum posuere."
     },
-    imagen: {
+    photo: {
       type: String,
       default: "Imagen de publicación"
     },
     tamañoImagen: {
       type: Number,
       default: 100 
+    },
+    guardada: {
+      type: Boolean,
+      default: false
+    },
+    id:{
+      type: Number,
+      default: 0
     }
-
   },
+
+  methods: {
+    deletePost (id){
+      axios.delete(`http://localhost/laravel_ecowave/example-app/public/api/publicacion/delete/${id}`)
+      .then(response => {
+        alert("Se ha eliminado el post correctamente");
+
+        window.location.href = 'http://localhost/EcoWave/dist/home.html';
+      });
+    },
+  },
+
   template:
   /*html*/
   `  
@@ -29,14 +52,16 @@ app.component('cards', {
       <div class="card-flex row g-0">
         <div class="col-md-7">
           <div class="card-body">
-            <p class="mb-2 camp-date">{{fecha}}</p>
-            <h5 class="fs-4 fw-bolder">{{titulo}}</h5>
-            <p class="card-text mb-2">{{descripcion}}</p>
-            <a class="camp-btn border-0 p-0" href="./detalle.html">Leer Más</a>
+            <p class="mb-2 camp-date">{{created_at}}</p>
+            <h5 class="fs-4 fw-bolder">{{title}}</h5>
+            <p class="card-text mb-2 overflow">{{description}}</p>
+            <a :href="'./detalle.html?id=' +id" class="camp-btn border-0 p-0">Leer Más</a>
+            <a :href="'./editpost.html?id=' + id" class="btn-editar ms-4" v-if="guardada">Editar</a>
+            <a href="#" class="btn-eliminar ms-4" v-if="guardada" @click="deletePost" >Eliminar</a>
           </div>
         </div>
-        <div class="col-md-5 overflow-hidden">
-          <img :src="imagen" class="img-fluid" style="{ width: tamañoImagen + '%' + ' !important' }" alt="fotografia de campaña">
+        <div class="col-md-5">
+          <img :src="photo" class="img-fluid" style="{ width: tamañoImagen + '%' + ' !important' }" alt="fotografia de campaña">
         </div>
       </div>
     </div>
